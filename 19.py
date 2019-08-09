ecd78bb7061da2b33df16427c6d972a714c3a8d5
priyatharshini23 <52314722+priyatharshini23@users.noreply.github.com>
2019-08-09 14:01:00 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,11 @@
+p=int(input())
+q=[]
+r=
+for i in range(p):
+ q.append(list(map(int,input().split())))
+for s in q:
+    for num in s:
+       r.append(num)
+r.sort()
+for i in r:
+    print(i,end=' ')
